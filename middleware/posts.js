d404949c03257bf1017500dc5017f22867478d3d
Richard Antao <richardmantao@gmail.com>
2020-04-09 16:41:50 -0400

    Updated properties

--- middleware/posts.js
+++ middleware/posts.js
@@ -2,7 +2,7 @@ const { body, validationResult } = require("express-validator");
 
 module.exports = (req, res, next) => {
     const errors = validationResult(req);
-    const { title, category } = req.body;
+    const { title, category, text, urlPath, password } = req.body;
 
     body(title, "Title received an invalid input")
         .exists().withMessage("Title is a required field")
@@ -12,15 +12,19 @@ module.exports = (req, res, next) => {
         .exists().withMessage("Category is a required field")
         .escape();
     
-    body(req.body.body, "Body received an invalid input")
+    body(text, "Body received an invalid input")
         .exists().withMessage("Body is a required field")
         .escape();
+    
+    body(urlPath, "URL path received an invalid input")
+        .exists().withMessage("URL path is a required field")
+        .escape();
 
-    if(password !== "123abc") {
+    if(password !== "abc123") {
         return res.status(401).json({ message: "Invalid password" });
     };
 
-    if(errors.isEmpty()) {
+    if(!errors.isEmpty()) {
         return res.status(400).json({ message: errors.msg });
     };
 
